031d95b37d74db2546f7540ec658ad85e52f2551
Nadim Attari <nadim.attari@gmail.com>
2024-04-15 01:29:29 +0400

    Use TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ Encore
     .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
