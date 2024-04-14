e77b9b6689d744e10fce880a4445c0bfdcf292e3
Nadim Attari <nadim.attari@gmail.com>
2024-04-15 01:10:00 +0400

    $ symfony composer require symfony/debug-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
