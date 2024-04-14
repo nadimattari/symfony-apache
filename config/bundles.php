b2a0f3b01684b94a80d3115104d2d3e2e6b47eda
Nadim Attari <nadim.attari@gmail.com>
2024-04-15 01:12:08 +0400

    $ symfony composer require --dev symfony/profiler-pack

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
